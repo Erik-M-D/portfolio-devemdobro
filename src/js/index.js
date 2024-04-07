cbac75b82b6da2f31089eb273a60426b9a0df23e
Erik-M-D <138533934+Erik-M-D@users.noreply.github.com>
2024-04-07 15:16:48 -0300

    adicao do botão menos

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,47 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoOcultarProjetos = document.querySelector('.btn-ocultar-projetos');
 
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
-    esconderBotao();
+    esconderBotaoMais();
+    adicionarBotaoMenos();
 });
 
-function esconderBotao() {
+botaoOcultarProjetos.addEventListener('click', () => {
+    ocultarProjetos();
+    esconderBotaoMenos();
+    adicionarBotaoMais();
+});    
+
+function esconderBotaoMais() {
     botaoMostrarProjetos.classList.add('remover');
 }
+
+function adicionarBotaoMenos() {
+    botaoOcultarProjetos.classList.remove('remover');
+}
+
+function esconderBotaoMenos() {
+    botaoOcultarProjetos.classList.add('remover');
+}
+
+function adicionarBotaoMais() {
+    botaoMostrarProjetos.classList.remove('remover');
+}
+
+
 
 function mostrarMaisProjetos() {
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
     });
 }
+
+function ocultarProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
+
+
